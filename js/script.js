daac343dac6262a4861cb632d3150f4ac1e46445
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-02-06 11:48:27 +0000

    live site - fully tested

--- js/script.js
+++ js/script.js
@@ -19,3 +19,51 @@ $('.section').waypoint(function(direction) {
      $('.site-nav__item--' + currentRegionId).addClass('is-active');
    }
  });
+
+
+
+ // ======================================
+ //    responsive image for main title
+ // ======================================
+
+// chooses which image should be loaded
+function whichTitle(option) {
+
+  // which image file should be used
+  var imageFile;
+  if (Modernizr.svgasimg) {
+    imageFile = '.svg';
+  } else {
+    imageFile = '.png';
+  }
+
+  // change image src to correct file
+  $('.js-title').attr('src', '/capital-region-usa/img/content/title-' + option + imageFile);
+
+}
+
+
+// checks if image is below the breakpoint
+function windowWidth() {
+  if ( $(window).width() < 650 ) {
+    // change title to vertical option
+    whichTitle('vertical');
+  } else {
+    // keep title to horizontal option
+    whichTitle('horizontal');
+  }
+}
+
+
+// run breakpoint check on page load
+windowWidth()
+
+
+// run breakpoint check on page load with limiter
+var resizeTimer;
+$(window).on('resize', function(e) {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    windowWidth();
+  }, 100);
+});
